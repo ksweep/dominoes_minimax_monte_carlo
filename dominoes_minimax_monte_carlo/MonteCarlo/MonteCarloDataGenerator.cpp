3d2e5b6e2310ffb3099181a67e914d7924c18fc6
Kevin Broom <kbroom@icloud.com>
2019-05-15 19:59:23 -0700

    sample over multiple threads

--- dominoes_minimax_monte_carlo/MonteCarlo/MonteCarloDataGenerator.cpp
+++ dominoes_minimax_monte_carlo/MonteCarlo/MonteCarloDataGenerator.cpp
@@ -12,7 +12,7 @@
 #include "CombinationListCreator.hpp"
 #include "MinimaxSearch.hpp"
 
-void MonteCarloDataGenerator::_updateChildCounts(const bool isMaxPlayer, const GameFoundation& gameFoundation, GameState& workState, IntVector& childCounts, IntPair& maxIndices) {
+void MonteCarloDataGenerator::_updateChildCounts(const bool isMaxPlayer, const GameFoundation& gameFoundation, GameState& workState, IntVector& childCounts) {
     int currentMinimaxValue = isMaxPlayer ? gameFoundation.negativeInfinity : gameFoundation.infinity;
     int bestChildIndex = -1;
     for (int i = 0; i < workState.children.size(); i++) {
@@ -25,15 +25,6 @@ void MonteCarloDataGenerator::_updateChildCounts(const bool isMaxPlayer, const G
     }
 
     childCounts[bestChildIndex] = childCounts[bestChildIndex] + 1;
-
-    if (bestChildIndex != maxIndices.first) {
-        if (childCounts[bestChildIndex] > childCounts[maxIndices.first]) {
-            maxIndices.second = maxIndices.first;
-            maxIndices.first = bestChildIndex;
-        } else if (childCounts[bestChildIndex] > childCounts[maxIndices.second]) {
-            maxIndices.second = bestChildIndex;
-        }
-    }
 }
 
 int MonteCarloDataGenerator::_getBestChildIndex(IntVector& childCounts) {
@@ -46,6 +37,29 @@ int MonteCarloDataGenerator::_getBestChildIndex(IntVector& childCounts) {
         }
     }
     return indexToUse;
+}
+
+void MonteCarloDataGenerator::_processSamples(GameState& workState, IntVector& childCounts, const int startIndex, const int endIndex, const IntVectorVector& indexCombos, const bool isMaxPlayer, const int opponentHandSize, const DominoPointerVector& possibleTiles, const GameFoundation& gameFoundation) {
+    for (int currentSampleNumber = startIndex; currentSampleNumber < endIndex; currentSampleNumber++) {
+        const IntVector& indexCombo = indexCombos[currentSampleNumber];
+        workState.children.clear();
+        // set opponent hand to the sample hand
+        assert(indexCombo.size() == opponentHandSize);
+        DominoPointerVector& opponentHand = isMaxPlayer ? workState.minPlayerHand : workState.maxPlayerHand;
+        opponentHand.clear();
+        for (int i = 0; i < opponentHandSize; i++) {
+            opponentHand.push_back(possibleTiles[indexCombo[i]]);
+        }
+        // alpha-beta this state. this will generate children and assign scores.
+        MinimaxData alphaBetaData = AlphaBetaMinimaxSearch::runOnState(workState, false);
+        assert(workState.children.size() > 0);
+        // find child with best score and increment its count
+        _updateChildCounts(isMaxPlayer, gameFoundation, workState, childCounts);
+        // if there is only one move (i.e. child), do not need to sample anymore since that's the move we must make
+        if (workState.children.size() == 1) {
+            break;
+        }
+    }
 }
 
 void MonteCarloDataGenerator::_addNewStateToPathAndUpdatePossibleTiles(const GameState& fromState, const GameState& toState, const GameState& mostRecentState, Game& game, MonteCarloData& data) {
@@ -123,11 +137,12 @@ void MonteCarloDataGenerator::_addNewStateToPathAndUpdatePossibleTiles(const Gam
 }
 
 MonteCarloData MonteCarloDataGenerator::generateData(const Game &game, const GameState &initialState, const int numberOfSamples) {
+    const int hardwareCores = std::thread::hardware_concurrency();
+
     MonteCarloData result;
+    result.path.push_back(initialState);
 
     Game workGame = game; // need a copy so we can update possible tiles
-
-    result.path.push_back(initialState);
 
     while (true) {
         const GameState& mostRecentState = result.path.back();
@@ -146,45 +161,33 @@ MonteCarloData MonteCarloDataGenerator::generateData(const Game &game, const Gam
 
         const int samplesToUse = std::min(numberOfSamples, (int)indexCombos.size());
 
-        GameState workState = GameState::stateForMonteCarloSampling(mostRecentState);
-
         // sample random hands to find best move
+        const int cores = samplesToUse > hardwareCores ? hardwareCores : 1;
+        const int segmentSize = samplesToUse / cores;
+        IntVectorVector childCountsByCore;
+        std::vector<GameState> workStatesByCore;
+        for (int i = 0; i < cores; i++) {
+            childCountsByCore.push_back(IntVector(game.gameFoundation.handSize, 0));
+            workStatesByCore.push_back(GameState::stateForMonteCarloSampling(mostRecentState));
+        }
+        std::vector<std::thread> threads;
+        for (int i = 0; i < cores; i++) {
+            const int startIndex = i * segmentSize;
+            const int endIndex = (i == cores - 1) ? samplesToUse : std::min(startIndex + segmentSize, samplesToUse);
+            threads.push_back(std::thread(_processSamples, std::ref(workStatesByCore[i]), std::ref(childCountsByCore[i]), startIndex, endIndex, std::ref(indexCombos), isMaxPlayer, opponentHandSize, std::ref(possibleTiles), std::ref(game.gameFoundation)));
+        }
+        for (int i = 0; i < threads.size(); i++) {
+            threads[i].join();
+        }
         IntVector childCounts(game.gameFoundation.handSize, 0);
-        IntPair maxIndices = std::make_pair(0, 1);
-
-        for (int currentSampleNumber = 0; currentSampleNumber < samplesToUse; currentSampleNumber++) {
-            const IntVector& indexCombo = indexCombos[currentSampleNumber];
-
-            workState.children.clear();
-
-            // set opponent hand to the sample hand
-            assert(indexCombo.size() == opponentHandSize);
-            DominoPointerVector& opponentHand = isMaxPlayer ? workState.minPlayerHand : workState.maxPlayerHand;
-            opponentHand.clear();
-            for (int i = 0; i < opponentHandSize; i++) {
-                opponentHand.push_back(possibleTiles[indexCombo[i]]);
-            }
-
-            // alpha-beta this state. this will generate children and assign scores.
-            MinimaxData alphaBetaData = AlphaBetaMinimaxSearch::runOnState(workState, false);
-            assert(workState.children.size() > 0);
-
-            // find child with best score and increment its count
-            _updateChildCounts(isMaxPlayer, game.gameFoundation, workState, childCounts, maxIndices);
-
-            // if the count of this child can't be exceeded by any other child, no need to keep sampling
-            const int remainingSamples = currentSampleNumber + 1;
-            if (childCounts[maxIndices.first] > childCounts[maxIndices.second] + (samplesToUse - remainingSamples)) {
-                break;
-            }
-
-            // if there is only one move (i.e. child), do not need to sample anymore since that's the move we must make
-            if (workState.children.size() == 1) {
-                break;
+        for (int i = 0; i < cores; i++) {
+            for (int j = 0; j < game.gameFoundation.handSize; j++) {
+                childCounts[j] = childCounts[j] + childCountsByCore[i][j];
             }
         }
 
         // get best move from all samples (will have highest count in childIndexCounts)
+        GameState& workState = workStatesByCore[0];
         const int bestChildIndex = _getBestChildIndex(childCounts);
         const GameState bestChildRemovingChildren = GameState::stateRemovingChildren(workState.children[bestChildIndex]);
 
--- dominoes_minimax_monte_carlo/MonteCarlo/MonteCarloDataGenerator.hpp
+++ dominoes_minimax_monte_carlo/MonteCarlo/MonteCarloDataGenerator.hpp
@@ -15,9 +15,10 @@ typedef std::vector<IntVector> IntVectorVector;
 
 class MonteCarloDataGenerator {
 private:
-    static void _updateChildCounts(const bool isMaxPlayer, const GameFoundation& gameFoundation, GameState& workState, IntVector& childCounts, IntPair& maxIndices);
+    static void _updateChildCounts(const bool isMaxPlayer, const GameFoundation& gameFoundation, GameState& workState, IntVector& childCounts);
     static int _getBestChildIndex(IntVector& childCounts);
     static void _addNewStateToPathAndUpdatePossibleTiles(const GameState& fromState, const GameState& toState, const GameState& mostRecentState, Game& game, MonteCarloData& data);
+    static void _processSamples(GameState& workState, IntVector& childCounts, const int startIndex, const int endIndex, const IntVectorVector& indexCombos, const bool isMaxPlayer, const int opponentHandSize, const DominoPointerVector& possibleTiles, const GameFoundation& gameFoundation);
 public:
     static MonteCarloData generateData(const Game& game, const GameState& initialState, const int numberOfSamples);
 };
